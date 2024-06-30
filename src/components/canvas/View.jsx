664d32e7210e57e786581f922b7be0d46a832f5b
Balakrishna K <kbalakrishna017@gmail.com>
2024-06-30 14:00:49 +0530

    Added env maps
    Need to correct path and loading ways

--- src/components/canvas/View.jsx
+++ src/components/canvas/View.jsx
@@ -1,21 +1,38 @@
 'use client'
 
 import { forwardRef, Suspense, useImperativeHandle, useRef } from 'react'
-import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
+import { OrbitControls, Environment, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
 import { Three } from '@/helpers/components/Three'
+import { useLoader, useThree } from '@react-three/fiber'
+import { TextureLoader, EquirectangularReflectionMapping } from 'three'
+
+function Environment1({ texture }) {
+  const { scene } = useThree()
+  texture.mapping = EquirectangularReflectionMapping
+  scene.background = texture
+
+  return null
+}
 
 export const Common = ({ color }) => {
   console.log('Common', color)
-  return(
-  
-  <Suspense fallback={null}>
-    {color && <color attach='background' args={[color]} />}
-    <ambientLight />
-    <pointLight position={[20, 30, 10]} intensity={3} decay={0.2} />
-    <pointLight position={[-10, -10, -10]} color='blue' decay={0.2} />
-    <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
-  </Suspense>
-)
+  const texture = useLoader(TextureLoader, '/envmaps/images/Environment-Map-Empty-Warehouse2K.jpg')
+  return (
+    <Suspense fallback={null}>
+      {color && <color attach='background' args={[color]} />}
+      <directionalLight intensity={6} color='white' position={[4, 5, 2]} />
+      <ambientLight intensity={2} />
+      <PerspectiveCamera makeDefault fov={60} position={[-6, 0, 6]} />
+      <OrbitControls
+        enableZoom={false}
+        minPolarAngle={Math.PI / 5}  // Set minimum polar angle
+        maxPolarAngle={Math.PI / 2.5}  // Set maximum polar angle
+        target={[0, 0, 0]}
+      />
+      <Environment1 texture={texture} />
+      <Environment files="/envmaps/hdr/Environment-Map-Empty-Warehouse2K.hdr" ground={{ height: 35, radius: 100, scale: 200 }} />
+    </Suspense>
+  )
 }
 
 const View = forwardRef(({ children, orbit, ...props }, ref) => {
