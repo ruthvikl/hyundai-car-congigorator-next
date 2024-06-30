82d558fb3e359426e8fd75abd8c365fe00468761
Ruthvik L Bhat <ruthvik.bhat@xtendr.co>
2024-06-30 11:09:45 +0530

    Update page.jsx

--- app/[car]/[trim]/interior/page.jsx
+++ app/[car]/[trim]/interior/page.jsx
@@ -37,40 +37,40 @@ export default function Page({ params }) {
         <h1 className='text-3xl text-center'>{car}</h1>
         <p className='text-center text-lg'>{trim}</p>
         <div className='mt-2 w-11/12 mx-auto relative rounded-xl h-4/5'>
-            <View orbit className='h-96 sm:h-48 sm:w-full'>
-                <Suspense fallback={null}>
-                <Duck route='/trim' scale={2} position={[0, -1.6, 0]} model={cars['IONIQ 5']['SE'].exteriorModel.model} />
-                <Common color={'gray'} />
-                </Suspense>
-            </View>
-            <div className='absolute bottom-5 z-10'>
-                <div className='flex flex-row justify-evenly overflow-x-auto px-2 py-1 rounded-full gap-5 w-11/12 bg-opacity-70 bg-gray-100 mx-auto'>
-                {Object.keys(cars['IONIQ 5']['SE'].exteriorColors).map(color => {
-                    return (
-                    <img
-                        key={color}
-                        alt={color}
-                        onClick={() => setSelectedColor(color)}
-                        src={`/colors/${cars['IONIQ 5']['SE'].exteriorColors[color].image}.png`}
-                    className={`w-1/12 h-1/12 lg:w-1/12 ${selectedColor === color ? 'border-2 border-white rounded-full' : ''}`}
-                    />
-                )})}
-                </div>
+          <View orbit className='h-96 sm:h-48 sm:w-full'>
+            <Suspense fallback={null}>
+            <Duck route='/trim' scale={2} position={[0, -1.6, 0]} model={cars['IONIQ 5']['SE'].exteriorModel.model} />
+            <Common color={'gray'} />
+            </Suspense>
+          </View>
+          <div className='absolute bottom-5 z-10'>
+            <div className='flex flex-row justify-evenly overflow-x-auto px-2 py-1 rounded-full gap-5 w-11/12 bg-opacity-70 bg-gray-100 mx-auto'>
+            {Object.keys(cars['IONIQ 5']['SE'].exteriorColors).map(color => {
+              return (
+              <img
+                key={color}
+                alt={color}
+                onClick={() => setSelectedColor(color)}
+                src={`/colors/${cars['IONIQ 5']['SE'].exteriorColors[color].image}.png`}
+              className={`w-1/12 h-1/12 lg:w-1/12 ${selectedColor === color ? 'border-2 border-white rounded-full' : ''}`}
+              />
+            )})}
             </div>
+          </div>
         </div>
         <div className='text-center border-2 py-2 flex flex-row border-black w-10/12 mx-auto font-[HyundaiSansHead-Regular]' >
             <span>
-                Select {selectedColor}
+              Select {selectedColor}
             </span>
             <span className=''>
             <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='2.5'
-                stroke='currentColor'
-                className='size-6'>
-                <path strokeLinecap='round' strokeLinejoin='round' d='m8.25 4.5 7.5 7.5-7.5 7.5' />
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth='2.5'
+              stroke='currentColor'
+              className='size-6'>
+              <path strokeLinecap='round' strokeLinejoin='round' d='m8.25 4.5 7.5 7.5-7.5 7.5' />
             </svg>
             </span>
         </div>
