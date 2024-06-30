9cfbc5fba3d9a5a3f3444ce64c1bb80f38d53f9c
Ruthvik L Bhat <ruthvik.bhat@xtendr.co>
2024-06-30 11:12:46 +0530

    Update Examples.jsx

--- src/components/canvas/Examples.jsx
+++ src/components/canvas/Examples.jsx
@@ -8,22 +8,6 @@ import { Line, useCursor, MeshDistortMaterial } from '@react-three/drei'
 import { useRouter } from 'next/navigation'
 import { cars } from '@/data/cars.js'
 
-export const trim = ({ route = '/', ...props }) => {
-  const router = useRouter()
-  const [hovered, hover] = useState(false)
-  useCursor(hovered)
-  return (
-    <mesh
-      onClick={() => router.push(route)}
-      onPointerOver={() => hover(true)}
-      onPointerOut={() => hover(false)}
-      {...props}>
-      <sphereGeometry args={[1, 64, 64]} />
-      <MeshDistortMaterial roughness={0.5} color={hovered ? 'hotpink' : '#1fb2f5'} />
-    </mesh>
-  )
-}
-
 export const Logo = ({ route = '/trim', ...props }) => {
   const { car } = props
   const router = useRouter()
