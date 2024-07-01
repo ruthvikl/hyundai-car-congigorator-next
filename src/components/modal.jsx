dbbf1c34235519f866561b7ecf288c3a3e7743f9
Ruthvik L Bhat <ruthvik.bhat@xtendr.co>
2024-07-01 20:24:15 +0530

    minor changes

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -3,10 +3,10 @@ export const Modal = ({visible , setVisibility, title, description}) => {
     setVisibility(false)
   }
   return(
-    <div className={`fixed top-0 left-0 w-full h-full fixed z-20 pointer-events-none ${visible ? 'fade-in' : 'fade-out'}`}>
-      <div className='text-white w-10/12 bg-black/60 rounded-lg absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2 p-5 text-center flex flex-col justidy-center'>
-        <p className='text-xl'>{title}</p>
-        <p>{description}</p>
+    <div className={`fixed w-full z-20 bottom-10 pointer-events-none ${visible ? 'fade-in' : 'fade-out'}`}>
+      <div className='text-white w-10/12 bg-black/60 rounded-lg mx-auto p-5 text-center flex flex-col justidy-center'>
+        <p className='text-lg'>{title}</p>
+        <p className='text-sm'>{description}</p>
         <div className="bg-white w-fit mx-auto text-black rounded-full px-2 py-0.5 pointer-events-auto" onClick={handleXClick}>X</div>
       </div>
     </div>
