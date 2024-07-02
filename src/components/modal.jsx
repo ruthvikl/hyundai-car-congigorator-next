ce61226a3484a1e857a51dbe4efe7927549a5c3d
Ruthvik L Bhat <ruthvik.bhat@xtendr.co>
2024-07-02 19:02:13 +0530

    Update modal.jsx

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -21,13 +21,13 @@ export const Modal = ({ visible, setVisibility, title, description }) => {
       aria-labelledby="modal-title"
       aria-describedby="modal-description"
     >
-      <div className="text-white w-9/12 bg-black/35 rounded-lg mx-auto p-2 text-center flex flex-col justify-center pointer-events-auto gap-2">
+      <div className="text-white w-9/12 bg-black/35 rounded-lg mx-auto p-2 text-center flex flex-col justify-center pointer-events-none gap-2">
         <div className="flex flex-col gap-2">
           <p className="text-sm" id="modal-title">{title}</p>
           <p className="text-xs" id="modal-description">{description}</p>
         </div>
         <button 
-          className="bg-white w-fit mx-auto text-black rounded-full px-2 py-0.5"
+          className="bg-white w-fit mx-auto text-black rounded-full px-2 py-0.5 pointer-events-auto"
           onClick={handleXClick}
           aria-label="Close"
         >
